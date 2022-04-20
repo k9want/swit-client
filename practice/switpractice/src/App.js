ab5cbde8d485564a0dc433392b453549d8f8e5ee
qma <zndwlrnjr26@daum.net>
2022-04-20 22:59:46 +0900

    기존 html css파일들 react에 옮기기

--- practice/switpractice/src/App.js
+++ practice/switpractice/src/App.js
@@ -1,12 +1,89 @@
+import React from "react";
+import Article from "./Articles";
 import "./App.css";
+import logo from "./assets/logo.png";
+import mainImg from "./assets/main-img.jpg";
 
 function App() {
   return (
     <div className="App">
-      <button type="button" className="btn btn-primary">
-        Primary
-      </button>
-      <a href="#!"> 하이</a>
+      <header>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12 header-line">
+              <h1 class="logo">
+                <a href="#!">
+                  <img src={logo} alt="SWIT" />
+                </a>
+              </h1>
+              <nav>
+                <h1>SWIT</h1>
+                <div class="header-btn">
+                  <a href="#!">
+                    <button class="non-border-btn non-show-btn">
+                      스터디 모집하기
+                    </button>
+                  </a>
+                  <a href="#!">
+                    <button class="non-border-btn">로그인</button>
+                  </a>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+      <section>
+        <div class="container">
+          <div class="row">
+            <div class="main">
+              <div class="col-12 col-md-5 main-title">
+                <h1>
+                  SWIT에서
+                  <br />
+                  같이 공부할
+                  <br />
+                  팀원을 찾아보세요
+                </h1>
+                <p>SWIT은 Study With의 약자입니다.</p>
+              </div>
+              <div class="col-12 col-md-7 main-img">
+                <img src={mainImg} alt="swit 이미지" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="category section">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-md-6">
+              <div class="category-btn-group">
+                <button class="category-btn">개발</button>
+                <button class="category-btn">취업</button>
+                <button class="category-btn">면접</button>
+                <button class="category-btn">공부</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="filter section">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 filter-btn-group">
+              <div>
+                <button class="non-border-btn">최신</button>
+                <button class="non-border-btn">인기</button>
+              </div>
+              <button class="non-border-btn non-show-btn filter-team">
+                팀원 찾는 글만 보기
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Article />;
     </div>
   );
 }
---
+++ practice/switpractice/src/Articles.js
@@ -0,0 +1,69 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import likeImg from "./assets/like.png";
+import viewImg from "./assets/eye.png";
+import commentImg from "./assets/comment.png";
+
+function Articles() {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+        setError(null);
+        setArticles(null);
+        // loading 상태를 true 로 바꿉니다.
+        setLoading(true);
+        const response = await axios.get(
+          "https://dev.studywith.site/articles/kind/1/recent"
+        );
+        setArticles(response.data); // 데이터는 response.data 안에 들어있습니다.
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+
+    fetchArticles();
+  }, []);
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!articles) return null;
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="row">
+          {articles.result.map(a => (
+            // <li key={article.articleId}> ({article.title})</li>
+            <div class="col-12 col-md-3">
+              <a href="#!" className="article" key={a.articleId}>
+                <p>{a.title}</p>
+                <div className="article-info-group">
+                  <div className="article-info">
+                    <img src={likeImg} alt="좋아요" />
+                    <p>{a.likeCount}</p>
+                  </div>
+                  <div className="article-info">
+                    <img src={viewImg} alt="조회수" />
+                    <p>{a.viewCount}</p>
+                  </div>
+                  <div className="article-info">
+                    <img src={commentImg} alt="댓글" />
+                    <p>{a.commentCount}</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Articles;
