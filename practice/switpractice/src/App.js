1773f7c9e000d49022b1766036edfc63e9aa7216
qma <zndwlrnjr26@daum.net>
2022-04-20 22:10:04 +0900

    swit 연습용 셋팅 완료

---
+++ practice/switpractice/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+      <a href="#!"> 하이</a>
+    </div>
+  );
+}
+
+export default App;
